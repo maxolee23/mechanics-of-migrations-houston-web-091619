eb257d638b30c7d0288bb5c6b0ba0960aea6c556
Max Lee <maxolee23@gmail.com>
2019-09-27 19:35:50 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,16 +1,5 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      title TEXT,
-      length INTEGER
-      )
-    SQL
-    ActiveRecord::Base.connection.execute(sql)
-  end
-
-   def change
+class CreateArtists < ActiveRecord::Migration[4.2]
+  def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
